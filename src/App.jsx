39f991f9daed41c8f34a9293f790a0283e7b7733
Mohammad Huzef Abdul Qayyum <141289932+huzef2002@users.noreply.github.com>
2025-04-08 18:43:00 +0530

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import SignUp from './component/SignUp'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
         <h1>Shaikh</h1>
         <p> E-commerce web</p>
       </div>
+      <SignUp/>
     </>
   )
 }
---
+++ src/component/SignUp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function SignUp() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <h1>Login page</h1>
+      <h1>SighOut Page</h1>
+    </div>
+  );
+}
+
+export default SignUp;
+
